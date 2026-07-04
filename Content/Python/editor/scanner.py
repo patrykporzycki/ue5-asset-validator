bdeea482bc0fe2ea6d3b4b546447a05ebcb954c7
patrykporzycki <74929239+patrykporzycki@users.noreply.github.com>
2026-07-04 23:24:19 +0200

    refactor: get assets by filtering registry, don't load every object

--- Content/Python/editor/scanner.py
+++ Content/Python/editor/scanner.py
@@ -1,20 +1,25 @@
 import unreal
 
 def scan_folders(paths):
-    EditorAssetLibrary = unreal.EditorAssetLibrary
-    assets = []
-    for path in paths:
-        asset_paths = EditorAssetLibrary.list_assets(path, recursive=True)
-        total_steps = len(asset_paths)
-        with unreal.ScopedSlowTask(total_steps, "Scanning Assets") as slow_task:
-            slow_task.make_dialog(can_cancel=True)
-            for asset_path in asset_paths:
-                if slow_task.should_cancel():
-                    unreal.log_warning("Scan cancelled")
-                    break
-                asset = EditorAssetLibrary.load_asset(asset_path)
-                if isinstance(asset, unreal.Texture2D):
-                    assets.append(asset)
-                slow_task.enter_progress_frame(1)
 
+    ar_filter = unreal.ARFilter(
+        package_paths=paths,
+        recursive_paths=True,
+        class_names=["Texture2D"]
+    )
+
+    registry = unreal.AssetRegistryHelpers.get_asset_registry()
+    asset_datas = registry.get_assets(ar_filter)
+    assets = []
+    total_steps = len(asset_datas)
+
+    with unreal.ScopedSlowTask(total_steps, "Scanning Assets") as slow_task:
+        slow_task.make_dialog(can_cancel=True)
+        for a in asset_datas:
+            if slow_task.should_cancel():
+                unreal.log_warning("Scan cancelled")
+                break
+            asset = a.get_asset()
+            assets.append(asset)
+            slow_task.enter_progress_frame(1)
     return assets
