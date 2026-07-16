1b24e69fd7355732c2a74a188048139836caa075
patrykporzycki <74929239+patrykporzycki@users.noreply.github.com>
2026-07-16 20:38:51 +0200

    feature: add more properties to static mesh and texture adapters

--- Content/Python/editor/validators/static_mesh_properties/static_mesh_adapter.py
+++ Content/Python/editor/validators/static_mesh_properties/static_mesh_adapter.py
@@ -7,8 +7,11 @@ class StaticMeshAdapter(AssetAdapter):
         dimensions = self.get_tag(asset_data, "EstTotalCompressedSize") or 0
         asset_properties = {
             "name": str(asset_data.asset_name),
+            "triangles": int(self.get_tag(asset_data, "Triangles") or 0),
+            "materials": int(self.get_tag(asset_data, "Materials") or 0),
             "lods": int(self.get_tag(asset_data, "LODs") or 0),
             "collisions": int(self.get_tag(asset_data, "CollisionPrims") or 0),
+            "nanite": bool(self.get_tag(asset_data, "NaniteEnabled") or 0),
             "estimated_size": int(dimensions),
         }
         return asset_properties
--- Content/Python/editor/validators/texture_properties/texture_adapter.py
+++ Content/Python/editor/validators/texture_properties/texture_adapter.py
@@ -15,6 +15,7 @@ class TextureAdapter(AssetAdapter):
             "resolution_x": int(dimensions[0]),
             "resolution_y": int(dimensions[1]),
             "compression": self.get_tag(asset_data, "CompressionSettings").upper(),
+            "alpha": self.get_tag(asset_data, "HasAlphaChannel").upper(),
             "srgb": self.get_tag(asset_data, "SRGB") == "True",
             "mipmaps": self.get_tag(asset_data, "MipGenSettings"),
             "estimated_size": int(dimensions[0]) * int(dimensions[1]) * 4
