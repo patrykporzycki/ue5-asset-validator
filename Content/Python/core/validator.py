65c953c883405d6c26e6bbd136dd4cbdd1381dda
patrykporzycki <74929239+patrykporzycki@users.noreply.github.com>
2026-07-31 16:46:38 +0200

    fix: delete using deep flag in validator

--- Content/Python/core/validator.py
+++ Content/Python/core/validator.py
@@ -3,8 +3,6 @@ from core.types import Check
 def validate(properties, rules, checks : list[Check]):
     results = []
     for check in checks:
-        if check.requires_deep:
-            continue
         alert = check.check(properties, rules)
         if alert is not None:
             results.append((alert, check))
